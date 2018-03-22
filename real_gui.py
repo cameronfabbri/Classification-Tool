a4a0b2af927f0a4b80d39a8ad3965959bb8d1c17
Hannah DuBois <duboi131@umn.edu>
2018-03-21 23:08:36 -0500

    “update”

--- real_gui.py
+++ real_gui.py
@@ -2,10 +2,10 @@ from tkinter import *
 from tkinter import filedialog
 import os
 from PIL import Image,ImageTk
-from time import gmtime, strftime  #used to make unique file names to write out to 
+from time import gmtime, strftime  #used to make unique file names to write out to
 
-import _pickle as pickle
-from sklearn import svm
+#import _pickle as pickle
+#from sklearn import svm
 
 '''
    need: I need to implement a way to start
@@ -21,8 +21,8 @@ from sklearn import svm
    - drop down box selecting active learning method (current would be 'random')
    - Say which class is which for buttons 1 and 2
    - load previous pickle file
-   - max image size so the image doesn't change window size
-   - 
+    x - max image size so the image doesn't change window size
+   -
 '''
 
 # this is how you load a pickle file. 'a' is then the dictionary that we saved
@@ -53,14 +53,14 @@ class classifier():
         self.root.title("Picture Classifier")
         self.root.geometry('500x300')
         self.quit = Button(self.root,text = "Quit", command = self.save)
-        self.quit.grid(column= 0,row = 0, sticky = E)
+        self.quit.grid(column= 3,row = 3)
         self.next = Button(self.root,text = "Next", command = self.getNext)
-        self.next.grid(column= 0, row = 1, sticky = E)
+        self.next.grid(column= 2, row = 3)
         self.prev = Button(self.root,text = "Previous", command = self.getPrev)
-        self.prev.grid(column = 0, row = 2, sticky = E)
+        self.prev.grid(column = 1, row = 3)
         self.load = Button(self.root,text = "Load", command = self.get_path)
-        self.load.grid(column = 0, row = 3, sticky = E)
-        self.label = Label(text= "No Image Loaded")
+        self.load.grid(column = 0, row = 3)
+        self.label = Label(text= "No Image Loaded",height = 15, width = 15)
         self.label.grid(row = 0, column = 1, columnspan = 10)
 
         def distorted(event):
@@ -69,8 +69,8 @@ class classifier():
         def normal(event):
             self.img_dict[self.img_list[self.index]]= 1
             self.getNext()
-        self.root.bind(2, distorted) # I should make it so it moves to the next image
-        self.root.bind(1, normal)  # after user presses button
+        self.root.bind(2, distorted)
+        self.root.bind(1, normal)
         self.index = 0
         self.img_dict = {}
         self.path = ""
@@ -85,7 +85,7 @@ class classifier():
     def load_img(self):
         im = Image.open(os.path.join(self.path, self.img_list[self.index]))
         photo = ImageTk.PhotoImage(im)
-        self.label.config(image=photo)
+        self.label.config(image=photo, height = 200, width = 300)
         self.label.image = photo
 
 
@@ -135,11 +135,11 @@ class classifier():
     #pushes all data out to text file
     #creates a unique name for each file
     def save(self):
-   
         pkl = open(self.path+'/labels.pkl', 'wb')
         data = pickle.dumps(self.img_dict)
         pkl.write(data)
         pkl.close()
-        
         self.root.destroy()
+
+
 c = classifier()
